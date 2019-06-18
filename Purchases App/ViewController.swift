a1b901ac1c30b8558b1011d5f18ffa075b0354d1
Saliou DJALO <djalosaliou@gmail.com>
2019-06-18 19:54:24 +0400

    Initial Commit

---
+++ Purchases App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Purchases App
+//
+//  Created by Saliou DJALO on 18/06/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
